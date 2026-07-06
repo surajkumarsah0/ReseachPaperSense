18e650a743654a9a24bdd83d8370aef74dfa8b1a
surajkumarsah0 <sah56880@gmail.com>
2026-07-06 11:33:50 +0545

    working in db connection

--- Backend/app/database/session.py
+++ Backend/app/database/session.py
@@ -9,14 +9,9 @@ load_dotenv()
 
 DATABASE_URL=os.getenv('DATABASE_URL')
 
-# 🚨 PRODUCTION ATTENTION: Yo address control mapping variables target string format framework chha
-# Timro Neon Dashboard ko connection configuration selection parameters tab ma gayera 
-# Connection String option parameters bata 'SQLAlchemy' type setup framework reference link string context yaha replace link lines lock gara.
 
-# Engine configuration link active mechanism line setups
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-# Dhara Generator instance connection session makers rules setup mappings lock
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Standard metadata dynamic tracker base object initialization instances
